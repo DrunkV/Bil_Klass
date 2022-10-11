39e37baf0340b8d294cde11cb349576e1d8448e8
Bruno <volturus@gmail.com>
2022-10-11 09:22:40 +0200

    Add Bil och Person Klasser

---
+++ Bil.py
@@ -0,0 +1,11 @@
+#Klass Bil
+
+class Bil:
+    def __init__(self, reg_num, fabrikat, årsmodell, tjänstevikt, motoreffekt, Person):
+        self.reg_num = reg_num
+        self.fabrikat = fabrikat
+        self.årsmodell = årsmodell
+        self.tjänstevikt = tjänstevikt
+        self.motoreffekt = motoreffekt
+        self.Person = Person
+
---
+++ Person.py
@@ -0,0 +1,7 @@
+#Klass Person
+
+class Person:
+    def __init__(self, förnamn, efternamn):
+        self.förnamn = förnamn
+        self.efternamn = efternamn
+
